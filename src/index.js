d3937048e7a68cbf843970b425897bbcd4bc7eb7
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2024-02-21 20:41:48 +0800

    finished the syling for the modal of adding new tasks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 import { projectAdd, projectAddConfirmation, addSettings, setActive} from './projects.js';
+import { addTask } from './tasks.js';
 
 let addProject = document.querySelector('#add-project');
 const menus_btn = document.querySelectorAll('.menu-btn');
@@ -11,7 +12,6 @@ addProject.addEventListener('click', function() {
     let getProjectElements = projectAdd();
     projectAddConfirmation(getProjectElements);
     addSettings(getProjectElements);
-
 });
 
 menus_btn.forEach(function(btn) {
@@ -20,3 +20,5 @@ menus_btn.forEach(function(btn) {
         taskTitleElement.textContent = btn.textContent;
     });
 });
+
+addTask();
--- src/tasks.js
+++ src/tasks.js
@@ -1,7 +1,15 @@
 function addTask() {
     const addTask = document.querySelector('#add-task');
+    const addTaskModal = document.querySelector('#add-task-modal');
+    let title = document.querySelector('#task-title');
+    let content = document.querySelector('#task-content');
+    let date = document.querySelector('#task-date');
 
     addTask.addEventListener('click', function() {
         console.log('Add Task');
+        
+        addTaskModal.style.display = 'flex';
     });
 }
+
+export { addTask };
